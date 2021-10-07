161723ff44604893b576aad5b33145ed04b80d0a
CrazyDiamond0 <popovici.alex96@gmail.com>
2021-10-07 23:41:45 +0300

    fetched data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+const axios = require("axios");
+
+axios
+  .post(
+    "https://api.graphql.jobs/",
+    {
+      query: `{
+  jobs{
+    title,
+    company{
+      name
+    },
+    postedAt,
+    createdAt,
+    updatedAt
+  }
+}`,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+  .then((res) => console.log(res.data));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className>Hello</div>;
 }
 
 export default App;
